2cb082aa892d0e3243ac974c1ebe0dd109baf860
ketan <mishraketan8425@gmail.com>
2022-02-21 19:08:23 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Quotes from "./Dashboard/Quotes/Quotes";
+import Buttons from "./Dashboard/Components/Buttons";
+import Footer from "./Dashboard/Components/Footer";
+import QuotesAuthor from "./Dashboard/Components/QuotesAuthor";
+import QuotesText from "./Dashboard/Components/QuotesText";
+
+function App() {
+  return (
+    <div className="App">
+      <Quotes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dashboard/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div id="footer">
+      <span>by </span>
+      <a rel="noopener noreferrer" href="https://github.com/Ketan2601">
+        Ketan
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
